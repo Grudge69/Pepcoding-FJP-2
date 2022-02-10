4665b3808eadc7b3e7102b58639f2ddf2b3482ed
Grudge69 <shubham.shubham.rawat090@gmail.com>
2022-02-10 16:48:14 +0530

    print all palindromic substrings

---
+++ DSA Questions/05-String, StringBuilder and ArrayList/Print All Palindromic Substrings.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static boolean isPalindrome(String s) {
+        // function to check if a string is palindrome or not
+        int i = 0;
+        int j = s.length() - 1;
+
+        while (i <= j) {
+            char ch1 = s.charAt(i);
+            char ch2 = s.charAt(j);
+
+            if (ch1 != ch2) {
+                return false;
+            } else {
+                i++;
+                j--;
+            }
+        }
+
+        return true;
+    }
+
+    public static void solution(String str) {
+
+        // getting all substrings in a string
+        for (int i = 0; i < str.length(); i++) {
+            for (int j = i + 1; j <= str.length(); j++) {
+                String ss = str.substring(i, j);
+                // checking individual substring if palindrome or not
+                if (isPalindrome(ss) == true) {
+                    System.out.println(ss);
+                }
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        String str = scn.next();
+        solution(str);
+    }
+
+}
