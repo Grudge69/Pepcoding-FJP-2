b83c8b3790a2aeaec033fa47bf2988fbd8e792b9
Grudge69 <shubham.shubham.rawat090@gmail.com>
2022-03-08 22:11:31 +0530

    highest wicket taker added

--- Web Dev/WEBSCRAPPING/highestWicketTaker.js
+++ Web Dev/WEBSCRAPPING/highestWicketTaker.js
@@ -4,7 +4,7 @@ let jsdom = require("jsdom"); //jsdom is used to read HTML
 let { JSDOM } = jsdom;
 
 //file system module
-// let fs = require("fs");
+let fs = require("fs");
 
 let url =
   "https://www.espncricinfo.com/series/afghanistan-in-bangladesh-2021-22-1299826/bangladesh-vs-afghanistan-1st-t20i-1299832/full-scorecard";
@@ -34,7 +34,7 @@ function parseHTML(html) {
   // let playerOfTheMatch = playerOfTheMatchTag.textContent;
   // console.log(playerOfTheMatch);
 
-  // WINNING TEAM
+  // 1. GET WINNING TEAM
   // get all the teams list
   let teams = MyDocument.querySelectorAll(".teams");
   // last element of the array has current match's teams list
@@ -43,11 +43,14 @@ function parseHTML(html) {
   let winningTeamName = currMatchTeam.querySelector(".team .name").textContent;
   // console.log("Winning Team : " + winningTeamName);
 
-  // all collapsibles -> team -> team details
-  let bothInningHtml = MyDocument.querySelectorAll(".Collapsible");
+  // 2. FILTERING FROM SCORECARD OF INDIVIDUAL TEAM
 
+  // all collapsibles -> team -> team details
+  let bothInningHtml = MyDocument.querySelectorAll(".Collapsible"); // GET BOTH TEAMS COLLAPSIBLE TABLE CONTAINING runs, wicket, etc
+
+  //from that getting the winning team's data
   for (let i = 0; i < bothInningHtml.length; i++) {
-    let singleInning = bothInningHtml[i];
+    let singleInning = bothInningHtml[i]; // 1 inning ke andar ka HTML
     // team name get
     let teamNameHtml = singleInning.querySelector("h5");
     // store the collapsible tables html in separate .html files
@@ -57,10 +60,42 @@ function parseHTML(html) {
     let teamNameRaw = teamNameHtml.textContent;
     let teamNameArr = teamNameRaw.split("INNINGS");
     let teamName = teamNameArr[0].trim();
-    if (winningTeamName == teamName) {
+    if (winningTeamName != teamName) {
+      // at the time BAN is batting AFG's bowler scorecard is shown and vice-versa
+
       // singleInnings[i] -> bowlers task complete
-      console.log("Winning team is " + teamName);
+      // console.log("Winnning team is " + finalTeamName);
       // code -> single inning
+      let rows = singleInning.querySelectorAll(".table.bowler tbody tr");
+      // -> jo rows belong karti kisko -> bowler
+      // loop -> tr bolwers -> use
+      let maxWickets = 0;
+      let maxWicketTaker;
+      for (let i = 0; i < rows.length; i++) {
+        let tdlength = rows[i].querySelectorAll("td").length;
+        // rmove commentry columns
+        if (tdlength > 1) {
+          // valid row -> belong kisi player ko
+          let bowlerRows = rows[i];
+          let tds = bowlerRows.querySelectorAll("td");
+          let name = tds[0].textContent;
+          let wickets = tds[4].textContent;
+          console.log(name + " " + wickets);
+          // compare(get highest wicket taker and name)
+          if (maxWickets < parseInt(wickets)) {
+            maxWickets = parseInt(wickets);
+            maxWicketTaker = name;
+          }
+        }
+      }
+
+      console.log(
+        "Highest wicket taker: " +
+          maxWicketTaker +
+          " with " +
+          maxWickets +
+          " wickets"
+      );
     }
   }
 }
