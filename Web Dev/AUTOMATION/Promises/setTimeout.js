82d66d359136037323b933987a03294cf4960c7c
Grudge69 <shubham.shubham.rawat090@gmail.com>
2022-03-24 22:39:43 +0530

    set timeout with 2 timeouts

--- Web Dev/AUTOMATION/Promises/setTimeout.js
+++ Web Dev/AUTOMATION/Promises/setTimeout.js
@@ -10,4 +10,15 @@ setTimeout(
   "Shubham"
 ); // WORKS ASYNCHRONOUSLY i.e. after console.log("before") & console.log("after")
 
+setTimeout(
+  function greet(greeting, name) {
+    console.log(`${greeting} my name is ${name}`);
+  },
+  5000,
+  "Hi",
+  "New Naam"
+);
+
+//TOTAL WAIT TIME IS 5sec as both set timeout aren't dependent on each other
+
 console.log("after");
