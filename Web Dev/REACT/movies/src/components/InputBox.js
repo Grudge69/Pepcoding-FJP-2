b3db87d2a8c2e0dece9afe632bcd2bf3d6acd522
Grudge69 <shubham.shubham.rawat090@gmail.com>
2022-05-13 13:52:16 +0530

    input box structure made

--- Web Dev/REACT/movies/src/components/InputBox.js
+++ Web Dev/REACT/movies/src/components/InputBox.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
 
 function InputBox() {
+  let [searchText, setSearchText] = React.useState("");
+  let [numberOfItems, setNumberOfItems] = React.useState(4);
+
+  const handleText = (e) => {
+    setSearchText(e.target.value);
+  };
+
+  const handleCount = (e) => {
+    setNumberOfItems(e.target.value);
+  };
+
   return (
-    <div>InputBox</div>
-  )
+    <>
+      {/* i will work on later */}
+      <button>New</button>
+      <input type="text" value={searchText} onChange={handleText}></input>
+      <input type="number" value={numberOfItems} onChange={handleCount}></input>
+    </>
+  );
 }
 
-export default InputBox
+export default InputBox;
