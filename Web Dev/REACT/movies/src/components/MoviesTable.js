dbae0c85a86fdcb1df25966c59f6e39687c02d0b
Grudge69 <shubham.shubham.rawat090@gmail.com>
2022-05-13 14:28:00 +0530

    movie data fetched from api

--- Web Dev/REACT/movies/src/components/MoviesTable.js
+++ Web Dev/REACT/movies/src/components/MoviesTable.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useEffect } from "react";
 
 function MoviesTable() {
+  const [isLoaded, setLoaded] = React.useState(true);
+  const [content, setContent] = React.useState(true);
+  //So, i will run only 1 time after first execution of return statement
+  useEffect(() => {
+    async function fetchData() {
+      //fetch is inbuilt feature of browser that makes the request to get data -> promise based
+      let response = await fetch(
+        "https://react-backend101.herokuapp.com/movies"
+      );
+      response = await response.json();
+      setLoaded(false);
+      setContent(JSON.stringify(response));
+    }
+    fetchData();
+  }, []);
+  //data
   return (
-    <div>MoviesTable</div>
-  )
+    <div>
+      {isLoaded == true ? (
+        <div className="font-bold">Loading...</div>
+      ) : (
+        <div>{content}</div>
+      )}
+    </div>
+  );
 }
 
-export default MoviesTable
+export default MoviesTable;
