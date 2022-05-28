d25be406bf443e4c9832b98268c2d7cb449c6841
Grudge69 <shubham.shubham.rawat090@gmail.com>
2022-05-28 17:36:15 +0530

    routing started

--- Web Dev/REACT/movies/src/Routing.js
+++ Web Dev/REACT/movies/src/Routing.js
@@ -1,27 +1,45 @@
-// import React from "react";
-// import { Route} from "react-router-dom";
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+import { Switch } from "react-router-dom";
 
-// function Home() {
-//   return <div>Home</div>;
-// }
+function Home() {
+  return <div>Home Page</div>;
+}
 
-// function Login() {
-//   return <div>Login</div>;
-// }
+function HomeSpecial() {
+  return <div>Home Special</div>;
+}
 
-// function PageNotFound() {
-//   return <div>404 Page Not Found!!!!!!!!!!!</div>;
-// }
+function Login() {
+  return <div>Login</div>;
+}
 
-// function Routing() {
-//   return (
-//     <>
-//       <div>Routing Example</div>
-//       <Route path="/home">
-//           <Home></Home>
-//         </Route>
-//     </>
-//   );
-// }
+function PageNotFound() {
+  return <div>404 Page Not Found!!!!!!!!!!!</div>;
+}
 
-// export default Routing;
+function Routing() {
+  return (
+    <>
+      <h2>Routing Example</h2>
+      <div className="border-b-2 mb-4">
+        <button className="bg-blue-500 mr-4">Login</button>
+        <button className="bg-blue-500">Home</button>
+      </div>
+      <Switch>
+        <Route path="/home/xyz">
+          <HomeSpecial></HomeSpecial>
+        </Route>
+        <Route path="/home" exact>
+          <Home></Home>
+        </Route>
+        <Redirect from="/" to="/home"></Redirect>
+        <Route>
+          <PageNotFound></PageNotFound>
+        </Route>
+      </Switch>
+    </>
+  );
+}
+
+export default Routing;
