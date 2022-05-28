5fb568c8daf8a1ee88f29e0a2097457e64b1a62e
Grudge69 <shubham.shubham.rawat090@gmail.com>
2022-05-28 18:16:39 +0530

    routing defined

--- Web Dev/REACT/movies/src/App.js
+++ Web Dev/REACT/movies/src/App.js
@@ -1,11 +1,29 @@
 //"rfce" to get boiler plate code
 import React from "react";
 import Home from "./components/Home";
+import Login from "./components/Login";
+import PageNotFound from "./components/PageNotFound";
+import New from "./components/New";
 import Routing from "./Routing";
+import { Route, Switch, Redirect } from "react-router-dom";
 function App() {
   return (
     <>
-      <Home></Home>
+      <Switch>
+        <Route path="/home">
+          <Home></Home>
+        </Route>
+        <Route path="/login">
+          <Login></Login>
+        </Route>
+        <Route path="/new">
+          <New></New>
+        </Route>
+        <Redirect from="/" to="/home"></Redirect>
+        <Route>
+          <PageNotFound></PageNotFound>
+        </Route>
+      </Switch>
     </>
     // <Routing></Routing>
   );
